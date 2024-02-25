2f8cc1090761b6d0816adf8b3bd5ebac77c9b6bf
josephmun <josephmun@pcp012450pcs.dhcp.missouri.edu>
2024-02-24 19:40:50 -0600

    test

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -5,7 +5,6 @@ public class Main {
         // Instantiate the VehicleManager
         VehicleManager vehicleManager = new VehicleManager();
 
-        // Attempt to initialize the stock from the CSV file
         boolean isStockInitialized = vehicleManager.initializeStock();
         if (isStockInitialized) {
             System.out.println("Vehicle stock has been successfully initialized.");
@@ -14,37 +13,21 @@ public class Main {
             return; // Exit if there is an issue with initializing stock
         }
 
-        // Display information about all vehicles
-        System.out.println("\nDisplaying information for all vehicles:");
+                System.out.println("\nDisplaying information for all vehicles:");
         vehicleManager.displayAllVehicleInformation();
 
-        // Display information about all cars
-        System.out.println("\nDisplaying information for all cars:");
+                System.out.println("\nDisplaying information for all cars:");
         vehicleManager.displayAllCarInformation();
 
-        // Display information about all trucks
         System.out.println("\nDisplaying information for all trucks:");
         vehicleManager.displayAllTruckInformation();
 
-        // Display information about all SUVs
         System.out.println("\nDisplaying information for all SUVs:");
         vehicleManager.displayAllSUVInformation();
 
-        // Display information about all motorbikes
+     
         System.out.println("\nDisplaying information for all motorbikes:");
         vehicleManager.displayAllMotorBikeInformation();
-
-        // Additional operations based on your homework requirements can be performed here
-        // For example, adding, removing vehicles, or displaying vehicles with specific conditions
-
-        // Example: Add a new vehicle (you need to replace this with actual vehicle details)
-        // Vehicle newVehicle = new Car("Brand", "Model", 2022, 25000, VehicleColor.BLACK, FuelType.GASOLINE, 30, 1500, 4, 55, StartMechanism.PUSHSTART);
-        // boolean isAdded = vehicleManager.addVehicle(newVehicle);
-        // if (isAdded) {
-        //     System.out.println("New vehicle added successfully.");
-        // } else {
-        //     System.out.println("Failed to add new vehicle.");
-        // }
 
         // Finally, save the updated vehicle list back to the CSV file
         boolean isListSaved = vehicleManager.saveVehicleList();
